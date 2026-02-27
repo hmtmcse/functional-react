5f67ff29fbd01832d26e1384c402b8d9047dc74e
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-02-27 22:21:26 +0600

    Working on

--- mfront-trial/runner/code/feature/example/example-ufr.tsx
+++ mfront-trial/runner/code/feature/example/example-ufr.tsx
@@ -1,7 +1,9 @@
 import {loadPage, RouteConfig} from "mfront";
 
 
-const Bismillah = loadPage(() => import("./bismillah"))
+const bismillah = loadPage(() => import("./bismillah"))
+const page2 = loadPage(() => import("./page2"))
+const page3 = loadPage(() => import("./page3"))
 
 const UI_BASE_URL = "/example"
 const API_BASE_URL = "api/v1/package-disburse-log/"
@@ -11,10 +13,14 @@ export default class ExampleUFR {
     static readonly api = {}
 
     static readonly ui = {
-        index: UI_BASE_URL
+        index: UI_BASE_URL,
+        page2: UI_BASE_URL + "/page2",
+        page3: UI_BASE_URL + "/page3",
     }
 
     static registerRoute(routerConfig: RouteConfig): void {
-        routerConfig.addPublicRoute({url: this.ui.index, content: Bismillah})
+        routerConfig.addPublicRoute({url: this.ui.index, content: bismillah})
+        routerConfig.addPublicRoute({url: this.ui.page2, content: page2})
+        routerConfig.addPublicRoute({url: this.ui.page3, content: page3})
     }
 }
