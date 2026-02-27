632693fd9dc90af4a472b94b46b47e134796f71d
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-02-27 22:16:47 +0600

    Working on

--- mfront-trial/dependencies/ui/mfront/code/router/use-request.ts
+++ mfront-trial/dependencies/ui/mfront/code/router/use-request.ts
@@ -1,12 +1,11 @@
 import {routerUseMatches, routerUseParams, routerUseSearchParams} from "@mfront/mfront-libs";
 
 export function useRequest() {
-    const params = routerUseParams();
     const [searchParams] = routerUseSearchParams();
     const matches = routerUseMatches();
     const current = matches.at(-1);
     return {
-        urlParams: params,
+        urlParams: routerUseParams(),
         queryParams: Object.fromEntries(searchParams.entries()),
         routeMeta: current,
     };
