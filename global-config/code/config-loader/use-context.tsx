ffd946dc86c8bc8ec20c8c541fb8abe2e1bdb33a
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2026-02-28 05:11:07 +0600

    Working on

--- global-config/code/config-loader/use-context.tsx
+++ global-config/code/config-loader/use-context.tsx
@@ -3,14 +3,14 @@ import AppConfig from "./app-config.tsx";
 
 type ContextDefinition = {
     config: AppConfig;
-    setConfig?: (newConfig: Partial<AppConfig>) => void;
+    setConfig: (newConfig: Partial<AppConfig>) => void;
 };
 
 export const useContext = create<ContextDefinition>((set) => ({
     config: new AppConfig(),
     setConfig: (newConfig) =>
-    set(() => ({
-      config: new AppConfig(),
-      ...newConfig,
-    })),
+        set(() => ({
+            config: new AppConfig(),
+            ...newConfig,
+        })),
 }))
